ab542782cf0f0c842feb5a14908ebf3562c84d29
Trishant <trishantjain4444@gmail.com>
2023-03-18 03:52:50 +0530

    endpoints for text generation and image generation

--- seotech-html/quesQuery.js
+++ seotech-html/quesQuery.js
@@ -1,101 +1,63 @@
-const axios = require('axios');
-require('dotenv').config();
-const apiKey = process.env.OPENAI_API_KEY;
-const fs = require('fs');
 
+function getQuery() {
+    const Query = document.getElementById('query').value;
+    return Query;
+}
 
+document.getElementById('form1').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const userQuery = getQuery();
 
+    const apiKey = "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";
+    const API_ENDPOINT = 'https://api.openai.com/v1/engines/text-davinci-002/completions';
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`,
+    };
+    const data = {
+        'prompt': userQuery,
+        'temperature': 0.5,
+        'max_tokens': 1000,
+        'top_p': 1,
+        'frequency_penalty': 0,
+        'presence_penalty': 0
+    };
 
-// Getting picture related to the query
-// Set up the API endpoint URL and parameters
-// const url = "https://api.unsplash.com/search/photos";
-// const accessKey = process.env.UNPLASH_API_KEY;
-
-// Define the API endpoint
-const API_ENDPOINT = 'https://api.openai.com/v1/engines/text-davinci-002/completions';
-
-// Define the prompt
-const prompt = 'how data is parse in server';
-const imgName = prompt.slice(0, 5);
-
-
-// Define the request headers
-const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${apiKey}`,
-};
-
-// Define the request data
-const data = {
-    'prompt': prompt,
-    'temperature': 0.5,
-    'max_tokens': 100,
-    'top_p': 1,
-    'frequency_penalty': 0,
-    'presence_penalty': 0
-};
-
-
-// fetch(apiUrl, {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json',
-//         'Authorization': `Bearer ${apiKey}`,
-//     },
-//     body: JSON.stringify({
-//         'model': 'image-alpha-001',
-//         'prompt': 'a cute cat sleeping on a pillow',
-//         'num_images': 1,
-//         'size': '256x256',
-//         'response_format': 'url'
-//     })
-// })
-
-// .then(response => response.json())
-// .then(data => {
-//   const imageUrl = data.data[0].url;
-//   const img = document.createElement('img');
-//   img.src = imageUrl;
-//   document.body.appendChild
-
-
-
-// Send the request to the OpenAI API
-axios.post(API_ENDPOINT, data, { headers })
-    .then(response => {
-        console.log(response.data.choices[0].text);
+    await fetch(API_ENDPOINT, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data)
     })
-    .catch(error => {
-        console.error(error);
-    });
-console.log('Answer Done')
-
-function generateImage(prompt) {
-    const imageUrl = 'https://api.openai.com/v1/images/generations';
-    axios.post(imageUrl, {
-        "model": "image-alpha-001",
-        "prompt": prompt,
-        "num_images": 1,
-        "size": "256x256",
-        "response_format": "url"
-    }, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`
-        },
-        responseType: 'arraybuffer'
-    })
-        .then(response => {
-            const imageData = Buffer.from(response.data, 'binary');
-            fs.writeFileSync(`${imgName}.png`, imageData);
-            console.log('File Saved')
-
-            // Use the image URL from the API response to modify your web page
-            // ...
+        .then(response => response.json())
+        .then(json => {
+            const result = json.choices[0].text;
+            document.getElementById('answer').innerHTML = result;
         })
         .catch(error => {
             console.error(error);
         });
-}
 
-generateImage(prompt);
+    const imgurl = 'https://api.openai.com/v1/images/generations';
+    await fetch(imgurl, {
+        method: 'POST',
+        headers: headers,
+
+        body: JSON.stringify({
+            "model": "image-alpha-001",
+            "prompt": userQuery,
+            "num_images": 1,
+            "size": "256x256",
+            "response_format": "url"
+        })
+    })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            const imageData = json.data[0].url;
+            console.log(imageData);
+            document.getElementById('imgGen').setAttribute('src', imageData);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
