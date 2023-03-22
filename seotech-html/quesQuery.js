e1751073acdb5c505993fdace51f1fc1901f9954
Trishant <trishantjain4444@gmail.com>
2023-03-22 23:52:12 +0530

    chanages in quesQuery file

--- seotech-html/quesQuery.js
+++ seotech-html/quesQuery.js
@@ -1,18 +1,16 @@
-
 function getQuery() {
     const Query = document.getElementById('query').value;
     return Query;
 }
-
 document.getElementById('form1').addEventListener('submit', async (event) => {
     event.preventDefault();
     const userQuery = getQuery();
 
-    const apiKey = "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";
+    const apiKey = "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";
     const API_ENDPOINT = 'https://api.openai.com/v1/engines/text-davinci-002/completions';
     const headers = {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`,
+        'Authorization': `Bearer ${apiKey}`
     };
     const data = {
         'prompt': userQuery,
@@ -30,34 +28,55 @@ document.getElementById('form1').addEventListener('submit', async (event) => {
     })
         .then(response => response.json())
         .then(json => {
-            const result = json.choices[0].text;
-            document.getElementById('answer').innerHTML = result;
+            const result2 = json.choices[0].text;
+            document.getElementById('answer').innerHTML = result2;
+
+            const imgurl = 'https://api.openai.com/v1/images/generations';
+
+            fetch(imgurl, {
+                method: 'POST',
+                headers: headers,
+
+                body: JSON.stringify({
+                    "model": "image-alpha-001",
+                    "prompt": result2,
+                    "num_images": 1,
+                    "size": "256x256",
+                    "response_format": "url"
+                })
+            })
+                .then(response => response.json())
+                .then(json => {
+                    const imageData = json.data[0].url;
+                    document.getElementById('imgGen').setAttribute('src', imageData);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
         })
         .catch(error => {
             console.error(error);
         });
 
-    const imgurl = 'https://api.openai.com/v1/images/generations';
-    await fetch(imgurl, {
-        method: 'POST',
-        headers: headers,
+    // const imgurl = 'https://api.openai.com/v1/images/generations';
+    // await fetch(imgurl, {
+    //     method: 'POST',
+    //     headers: headers,
 
-        body: JSON.stringify({
-            "model": "image-alpha-001",
-            "prompt": userQuery,
-            "num_images": 1,
-            "size": "256x256",
-            "response_format": "url"
-        })
-    })
-        .then(response => response.json())
-        .then(json => {
-            console.log(json);
-            const imageData = json.data[0].url;
-            console.log(imageData);
-            document.getElementById('imgGen').setAttribute('src', imageData);
-        })
-        .catch(error => {
-            console.error(error);
-        });
+    //     body: JSON.stringify({
+    //         "model": "image-alpha-001",
+    //         "prompt": result,
+    //         "num_images": 1,
+    //         "size": "256x256",
+    //         "response_format": "url"
+    //     })
+    // })
+    //     .then(response => response.json())
+    //     .then(json => {
+    //         const imageData = json.data[0].url;
+    //         document.getElementById('imgGen').setAttribute('src', imageData);
+    //     })
+    //     .catch(error => {
+    //         console.error(error);
+    //     });
 });
